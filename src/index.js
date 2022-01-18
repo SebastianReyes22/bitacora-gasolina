b7bbe1f8f69a4f0ad9fa3f4bcba7530e897f52fc
Noe S. Reyes Miramontes <sebasreyes935@hotmail.com>
2022-01-18 16:18:43 -0600

    Styles update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
